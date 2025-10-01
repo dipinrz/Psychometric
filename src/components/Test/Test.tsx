3ec8117b39f3ca68114669eb15c8bd7efc2bc5b5
dipinrz <dipindas.ah@reizend.ai>
2025-10-01 17:03:37 +0530

    proxy issue fix

--- src/components/Test/Test.tsx
+++ src/components/Test/Test.tsx
@@ -37,7 +37,7 @@ const AnomalyDetectionAnalytics = () => {
 
     try {
       const result = await axios.post(
-        "/api/predict",
+        "http://51.112.148.129:8089/predict",
         formData,
         {
           headers: {
