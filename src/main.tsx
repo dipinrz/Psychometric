8e349a0d41b8ce856891e0782c8a012fcb7ab939
dipinrz <dipindas.ah@reizend.ai>
2025-10-01 23:56:54 +0530

    test datas

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import AnomalyDetectionResults from "./components/Test/Test.tsx";
 import PsychometricTestAnalytics from "./components/Psycomatric.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <App />
-     */}
+    {/* <App /> */}
+
     <PsychometricTestAnalytics />
   </StrictMode>
 );
