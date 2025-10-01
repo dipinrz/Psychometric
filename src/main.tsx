a372159326a3896535eb1c3d98d29bc92248a9af
dipinrz <dipindas.ah@reizend.ai>
2025-10-01 22:49:44 +0530

    all

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import AnomalyDetectionResults from "./components/Test/Test.tsx";
+import PsychometricTestAnalytics from "./components/Psycomatric.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {/* <App />
+     */}
+    <PsychometricTestAnalytics />
   </StrictMode>
 );
